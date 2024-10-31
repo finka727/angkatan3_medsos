f265565d9725ef733670f88617f382c5f7f6064a
finka727 <finkaaa21@gmail.com>
2024-10-31 15:12:54 +0700

    add: register medsos

---
+++ inc/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg mb-5" style="background-color: #9370DB;">
+    <div class="container-fluid">
+      <!-- <a class="navbar-brand" href="img/logo.png"></a> -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link text-light" href="index.php">Dashboard</a>
+          <a class="nav-link text-light" href="?pg=user">Manage Accounts</a>
+          <a class="nav-link text-light" href="?pg=level">Level</a>
+          <a class="nav-link text-light" href="?pg=kategori">Category</a>
+          <a class="nav-link text-light" href="?pg=buku">Buku</a>
+          <a class="nav-link text-light" href="?pg=peminjaman">Peminjaman</a>
+          <a class="nav-link text-light" href="?pg=pengembalian">Pengembalian</a>
+          <a class="nav-link text-light" href="?pg=anggota">Anggota</a>
+        </div>
+        <a href="keluar.php" class="btn btn-danger btn-sm ms-auto"><i class="fa-solid fa-right-from-bracket"></i> Keluar</a>
+      </div>
+    </div>
+  </nav>
